f0d0cd7593ca9f55ab4b0f4230061132ad285a1d
Abu-muaz <plshireseid@gmail.com>
2025-12-06 11:39:55 +0300

    feat : add resend verification

---
+++ frontend/src/pages/ResendVerification.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ResendEmail from "../components/auth/ResendEmail";
+
+const ResendVerification = () => {
+  return <ResendEmail />;
+};
+
+export default ResendVerification;
