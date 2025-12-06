2478af6eebee73c66f4bdc617fb4a4eb14185ec5
Abu-muaz <plshireseid@gmail.com>
2025-12-06 11:50:40 +0300

    feat : add layout

---
+++ frontend/src/components/common/Layout.jsx
@@ -0,0 +1,62 @@
+// import React from "react";
+// import { Outlet } from "react-router-dom";
+// import Header from "./Header";
+// import Footer from "./Footer";
+// import LoadingSpinner from "./LoadingSpinner";
+// import { useSelector } from "react-redux";
+// import "./Layout.css";
+
+// const Layout = () => {
+//   const { loading } = useSelector((state) => state.auth);
+
+//   if (loading) {
+//     return <LoadingSpinner />;
+//   }
+
+//   return (
+//     <div className="layout">
+//       <Header />
+//       <main className="main-content">
+//         <Outlet />
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import LoadingSpinner from "./LoadingSpinner";
+import { useSelector } from "react-redux";
+
+const Layout = ({ children }) => {
+  const { loading } = useSelector((state) => state.auth);
+
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        {loading ? <LoadingSpinner /> : children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
