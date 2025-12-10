32dbdd3449007da88db25675e5236a56332aea25
Abu-muaz <plshireseid@gmail.com>
2025-12-10 21:32:02 +0300

    fix : cors error

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ const allowedOrigins =
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:3000",
-        "https://my-appointmentbackend.vercel.app",
+        "https://my-appointment-app-eta.vercel.app",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         process.env.CLIENT_URL,
